01b39da6d6b6456a2a0c02d3a2fe07f45d801510
Kasidet Sae-oueng <kasidet.jack@gmail.com>
2025-03-17 15:43:28 +0700

    fix

--- app/attractions/page.js
+++ app/attractions/page.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { 
-  Card, CardActions, CardContent, CardMedia, Button, 
-  Typography, Grid 
+import {
+  Card, CardActions, CardContent, CardMedia, Button, Typography, Grid
 } from '@mui/material'
 
 export async function getData() {
@@ -30,7 +29,8 @@ export default async function page() {
                 title={attraction.name}
               />
               <CardContent>
-                <Typography gutterBottom variant="h6" component="div">
+                <Typography gutterBottom variant="h6"
+                  component="div">
                   {attraction.name}
                 </Typography>
                 <Typography variant="body2" color="text.secondary" noWrap>
